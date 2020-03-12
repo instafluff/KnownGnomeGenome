ce126734d01fb1962c54f11a48be134e8c1fc192
instafluff <instaflufftv@gmail.com>
2020-03-12 05:22:48 -0700

    Add virus data reference

--- genomerate.js
+++ genomerate.js
@@ -1,19 +1,36 @@
 const fs = require( "fs" );
 
-const genomeLines = fs.readFileSync( "sequences.fasta" ).toString().split( "\n" );
+const referenceLines = fs.readFileSync( "sequences.csv" ).toString().split( "\n" ).filter( x => !!x );
+let virusData = {};
+let virusColumns = referenceLines[ 0 ].split( "," );
+for( let i = 1; i < referenceLines.length; i++ ) {
+	let columns = referenceLines[ i ].split( "," );
+	virusData[ columns[ 0 ] ] = {};
+	for( let j = 0; j < virusColumns.length; j++ ) {
+		virusData[ columns[ 0 ] ][ virusColumns[ j ] ] = columns[ j ].trim();
+	}
+}
 
+// console.log( virusData );
+
+const genomeLines = fs.readFileSync( "sequences.fasta" ).toString().split( "\n" );
 let viruses = {};
 
 let currentVirus = null;
 for( let i = 0; i < genomeLines.length; i++ ) {
 	if( genomeLines[ i ].startsWith( ">" ) ) {
 		let parts = genomeLines[ i ].replace( ">", "" ).split( "|" );
-		let id = parts[ 0 ];
+		let id = parts[ 0 ].trim();
 		let name = parts[ 1 ];
 		currentVirus = id;
 		viruses[ id ] = {
 			id: id,
 			name: name,
+			species: virusData[ id ].Species,
+			genus: virusData[ id ].Genus,
+			family: virusData[ id ].Family,
+			location: virusData[ id ].Geo_Location,
+			host: virusData[ id ].Host,
 			genome: ""
 		};
 	}
@@ -30,14 +47,19 @@ Object.keys( viruses ).forEach( v => {
 	// console.log( viruses[ v ].name, genomeToCoord( viruses[ v ].genome ) );
 	output[ v ] = {
 		name: viruses[ v ].name,
+		species: viruses[ v ].species,
+		genus: viruses[ v ].genus,
+		family: viruses[ v ].family,
+		location: viruses[ v ].location,
+		host: viruses[ v ].host,
 		coord: genomeToCoord( viruses[ v ].genome )
 	};
 });
 fs.writeFileSync( "web/viruses.json", JSON.stringify( output ) );
 console.log( output );
-// console.log( viruses );
 
 function genomeToCoord( genome ) {
+	const coordScale = 10;
 	let x = 0, y = 0;
 	let a = 0, g = 0, c = 0, t = 0;
 	genome.split( "" ).forEach( ( n, i ) => {
@@ -45,24 +67,22 @@ function genomeToCoord( genome ) {
 		let interval = 1 / ( i + 1 );
 		switch( n ) {
 			case "A":
-				x += interval;//1 / ( i + 1 );
+				x += interval;
 				a += interval;
 				break;
 			case "G":
-				// x -= interval;//1 / ( i + 1 );
-				y -= interval;//1 / ( i + 1 );
+				y -= interval;
 				g += interval;
 				break;
 			case "C":
-				y += interval;//1 / ( i + 1 );
+				y += interval;
 				c += interval;
 				break;
 			case "T":
-				// y -= interval;//1 / ( i + 1 );
-				x -= interval;//1 / ( i + 1 );
+				x -= interval;
 				t += interval;
 				break;
 		}
 	});
-	return { x, y, a, g, c, t };
+	return { x, y, a: a / coordScale, g: g / coordScale, c: c / coordScale, t: t / coordScale };
 }
